e9a87956e9034623dafeb25afa06ea84ec6e8cea
Anand997758 <96283755+Anand997758@users.noreply.github.com>
2022-07-22 09:39:56 +0530

    this is modified one file

---
+++ describe-autoscaling/instance-describe.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import boto3
+
+AWS_REGION = "ap-southeast-2"
+EC2_RESOURCE = boto3.resource('ec2', region_name=AWS_REGION)
+INSTANCE_STATE = 'running'
+
+
+instances = EC2_RESOURCE.instances.filter(
+    Filters=[
+        {
+            'Name': 'instance-state-name',
+            'Values': [
+                INSTANCE_STATE
+            ]
+        }
+    ]
+)
+
+print(f'Instances in state "{INSTANCE_STATE}":')
+
+for instance in instances:
+    print(f'  - Instance ID: {instance.id}')
